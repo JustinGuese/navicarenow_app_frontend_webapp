6edf10638c89fa94053e8e18592d118035f9d602
Justin Guese <guese.justin@gmail.com>
2026-01-14 14:06:16 +0100

    Update webapp

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "06c73ea01e276ec931d8ea125b499c0b",
+const RESOURCES = {"flutter_bootstrap.js": "2853f0e25cbed6ab578ca11e6fc710b9",
 "version.json": "12a8df2593d3faa3b852d6d1a084be08",
 "index.html": "15a2bf8e37b7042622c155426ad38569",
 "/": "15a2bf8e37b7042622c155426ad38569",
-"main.dart.js": "67a2f845692679dd822ec7e57f5f22a3",
+"main.dart.js": "b39de93dd885240ea554845792271358",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
